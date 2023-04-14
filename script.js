a235e1d3eb759201d42603784ea6a18da371655a
Pedro Teixeira <67431476+Texeraaa@users.noreply.github.com>
2023-04-14 12:17:21 -0300

    screen swap

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const arrayButton = document.querySelectorAll('#botao')
+const send = document.getElementById('send')
 
 arrayButton.forEach((item) => {
     item.addEventListener('click', function(e){
@@ -13,5 +14,22 @@ arrayButton.forEach((item) => {
             this.classList.toggle('text-white')
         })
     })
+})
+const start = document.getElementById('start')
+const thank = document.getElementById('thank')
+send.addEventListener('click', () => {
+    console.log(start,thank)
+    start.classList.replace('flex', 'hidden')
+    thank.classList.replace('hidden', 'flex')
+    // start.classList.add('hidden')
+    // thank.classList.remove('hidden')
+    // thank.classList.add('flex flex-col gap-6')
     
 })
+
+
+let num = 1
+// console.log(arrayButton[4])
+document.querySelector('#label').innerHTML = `
+<p class="text-orange opacity-20 text-base">You selected ${num} out of 5</p>
+`
