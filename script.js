de9cc3492cf84e2d1c1a26088325868d99a26e3d
Pedro Teixeira <67431476+Texeraaa@users.noreply.github.com>
2023-04-14 11:04:34 -0300

    sistema de rating

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementById('botao').addEventListener("click", function(event))
+const arrayButton = document.querySelectorAll('#botao')
+
+arrayButton.forEach((item) => {
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+
+        arrayButton.forEach((i) => {
+            if(i != this){
+                i.classList.remove('bg-medium-gray')
+                i.classList.remove('text-white')
+            }
+            this.classList.toggle('bg-medium-gray')
+            this.classList.toggle('text-white')
+        })
+    })
+    
+})
