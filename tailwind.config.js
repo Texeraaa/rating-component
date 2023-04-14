5fc67867cb817e8c6433ab66df43e1a080d58b08
Pedro Teixeira <67431476+Texeraaa@users.noreply.github.com>
2023-04-14 14:33:46 -0300

    bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         'orange': 'hsl(25, 97%, 53%)',
         'white': 'hsl(0, 0%, 100%)',
         'light-gray': 'hsl(217, 12%, 63%)',
-        'medium-gray': 'hsl(216, 12%, 54%)',
+        'medium-gray': 'hsl(216, 13%, 54%)',
         'str-gray': 'rgba(38,46,57,255)',
         'dark-blue': 'rgba(24,30,40,255)',
         'str-dark-blue-2': 'rgba(20,20,29,255)',
